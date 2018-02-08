dbb43bd50087389a9d31cb81f68a4308f99da925
Xander Bindt <xbindt@hotmail.com>
2018-02-08 15:19:05 +0100

    no message

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //  OpenShift sample Node serverlication
 const express = require('express'),
     morgan  = require('morgan'),
+    server     = express(),
     next = require('next');
     
 Object.assign=require('object-assign');
@@ -9,49 +10,23 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-//server.engine('html', require('ejs').renderFile);
-//server.use(morgan('combined'))
+server.engine('html', require('ejs').renderFile);
+server.use(morgan('combined'))
 
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
-/*
+
 server.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    const col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
+  
     res.render('index.html', { pageCountMessage : null});
-  }
+
 });
 
-server.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
+
 
 // error handling
 server.use(function(err, req, res, next){
@@ -59,34 +34,6 @@ server.use(function(err, req, res, next){
   res.status(500).send('Something bad hserverened!');
 });
 
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
 
 server.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
-*/
-app.prepare()
-.then(() => {
-    const server = express();
-    server.use(morgan('combined'))
-
-    server.get('/p/:id', (req, res) => {
-        const actualPage = '/post'
-        const queryParams = { id: req.params.id }
-        app.render(req, res, actualPage, queryParams)
-    })
-
-    server.get('*', (req, res) => {
-        return handle(req, res)
-    })
-
-    server.listen(port, ip, (err) => {
-        if (err) throw err
-        console.log('> Ready on http://localhost:3000')
-    })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})

33854caead0efac46fa28e88d6b6e6e8f1d1cfee
Xander Bindt <xbindt@hotmail.com>
2018-02-08 14:53:50 +0100

    package changed

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 //  OpenShift sample Node serverlication
 const express = require('express'),
-    server     = express(),
     morgan  = require('morgan'),
     next = require('next');
     
@@ -14,52 +13,9 @@ const handle = app.getRequestHandler()
 //server.use(morgan('combined'))
 
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  const mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
 
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
-}
-const db = null,
-    dbDetails = new Object();
-
-const initDb = function(callback) {
-  if (mongoURL == null) return;
-
-  const mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
-    }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
-  });
-};
 /*
 server.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
@@ -112,7 +68,8 @@ console.log('Server running on http://%s:%s', ip, port);
 */
 app.prepare()
 .then(() => {
-   
+    const server = express();
+    server.use(morgan('combined'))
 
     server.get('/p/:id', (req, res) => {
         const actualPage = '/post'
@@ -133,5 +90,3 @@ app.prepare()
   console.error(ex.stack)
   process.exit(1)
 })
-
-module.exports = server ;

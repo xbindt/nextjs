aa23bac61075e3f06574fca2ed0596d87ac75ca8
Xander Bindt <xbindt@hotmail.com>
2018-02-08 15:59:20 +0100

    no message

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
 //  OpenShift sample Node serverlication
-const express = require('express'),
-    morgan  = require('morgan'),
-    server     = express(),
-    next = require('next');
-    
-Object.assign=require('object-assign');
+const express = require('express')
+const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
-
-server.engine('html', require('ejs').renderFile);
-server.use(morgan('combined'))
+    
 
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
 
-server.get('/', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  
-    res.render('index.html', { pageCountMessage : null});
+app.prepare()
+.then(() => {
+    const server = express()
 
-});
+    server.get('/p/:id', (req, res) => {
+        const actualPage = '/post'
+        const queryParams = { id: req.params.id }
+        app.render(req, res, actualPage, queryParams)
+    })
 
+    server.get('*', (req, res) => {
+        return handle(req, res)
+    })
 
-
-// error handling
-server.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad hserverened!');
-});
-
-
-server.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
+    server.listen(port, ip, (err) => {
+        if (err) throw err
+        console.log('> Ready on http://localhost:3000')
+    })
+})
+.catch((ex) => {
+  console.error(ex.stack)
+  process.exit(1)
+})

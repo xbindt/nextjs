aa000c65986fd6bb5ad8262559c47b6e36f2b8b5
Xander Bindt <xbindt@hotmail.com>
2018-03-08 17:49:52 +0100

    versie update

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,8 @@
-import Layout from '../components/MyLayout.js'
+import Layout from '../components/Layout.js'
 
 export default () => (
     <Layout>
        <p>This is the about page</p>
+       
     </Layout>
 )
